064ca92aa7668078d881ae74b7835fa3e50aa33b
Joethepic <60947591+Joethepic@users.noreply.github.com>
2024-07-31 14:03:23 -0500

    intial release :)

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+    var rand = new Random();
+    StreamReader inputfile;
+    inputfile = File.OpenText("Objectives.txt");
+    string[] objectives = inputfile.ReadToEnd().Split('\n');
+    int i = objectives.Length;
+    int cobjective = 0;
+    while (cobjective < 25 && cobjective < objectives.Length)
+    {
+        int RObjective = rand.Next(0, i);
+        Console.WriteLine(objectives[RObjective]);
+        cobjective++;
+    }
