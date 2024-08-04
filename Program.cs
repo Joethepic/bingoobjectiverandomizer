d22064f46e9a58ae7e001d356d7102f81e26d3cb
Joethepic <60947591+Joethepic@users.noreply.github.com>
2024-08-04 12:57:51 -0500

    Let's you change the number of objectives
    This allows the number of objectives to be what you want not always 25

--- Program.cs
+++ Program.cs
@@ -4,10 +4,12 @@
     inputfile = File.OpenText("Objectives.txt");
     string[] objectives = inputfile.ReadToEnd().Split('\n');
     int i = objectives.Length;
-    int cobjective = 0;
-    while (cobjective < 25 && cobjective < objectives.Length)
-    {
-        int RObjective = rand.Next(0, i);
-        Console.WriteLine(objectives[RObjective]);
-        cobjective++;
-    }
+Console.WriteLine("Type how many objectives you would like to have rolled from your objective list");
+int nobjectives = Convert.ToInt32(Console.ReadLine());
+int cobjective = 0;
+while (cobjective < nobjectives && cobjective < objectives.Length)
+{
+    int RObjective = rand.Next(0, i);
+    Console.WriteLine(objectives[RObjective]);
+    cobjective++;
+}
