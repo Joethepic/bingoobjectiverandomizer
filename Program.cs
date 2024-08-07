2bceeb8359697e8ba6dab9acc4d05321ce02c4b6
Joethepic <60947591+Joethepic@users.noreply.github.com>
2024-08-07 10:23:11 -0500

    Let's you add objectives without opening the file

--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
 ﻿
     var rand = new Random();
-    StreamReader inputfile;
-    inputfile = File.OpenText("Objectives.txt");
-    string[] objectives = inputfile.ReadToEnd().Split('\n');
-    int i = objectives.Length;
+Console.WriteLine("Please enter the objectives you would like to add to the list please press enter after each objective. Type 'exit' when you have finished all your objectives");
+string objective = Console.ReadLine();
+StreamWriter outputfile;
+outputfile = File.AppendText("Objectives.txt");
+while (objective != "exit")
+{
+    outputfile.WriteLine(objective);
+    objective = Console.ReadLine();
+}
+    outputfile.Close();
+StreamReader inputfile;
+inputfile = File.OpenText("Objectives.txt");
+string[] objectives = inputfile.ReadToEnd().Split('\n');
+int i = objectives.Length;
 Console.WriteLine("Type how many objectives you would like to have rolled from your objective list");
 int nobjectives = Convert.ToInt32(Console.ReadLine());
 int cobjective = 0;
@@ -13,3 +23,4 @@ while (cobjective < nobjectives && cobjective < objectives.Length)
     Console.WriteLine(objectives[RObjective]);
     cobjective++;
 }
+inputfile.Close(); 
